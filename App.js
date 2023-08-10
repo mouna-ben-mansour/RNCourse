cf9a88eb65b106f815ebe82aed0bc5dea4543e43
mouna-ben-mansour <benmansour.mouna2@gmail.com>
2023-08-10 17:52:38 +0100

    strat with flexBox

--- App.js
+++ App.js
@@ -1,34 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
 export default function App() {
     return (
         <View style={styles.container}>
-            <View style={styles.container}>
-                <Text style={styles.dummyText}>My app title</Text>
-                <Text style={{margin: 16, borderBottomWidth:2, borderBottomColor: 'red', padding: 16}}>Hello!</Text>
-                <Button title='Press me' onPress={()=>Alert.alert('Simple button pressed')}/>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder='Your course goal!'/>
+                <Button title="Add Goal"/>
             </View>
             <View style={styles.separator} ></View>
+            <View >
+                <Text>List of goals...</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        padding: 50
     },
     separator: {
         marginVertical: 8,
         borderBottomColor: '#737373',
         borderBottomWidth: StyleSheet.hairlineWidth,
     },
-    dummyText: {
-        margin: 16,
-        borderWidth:2,
-        borderColor: 'blue',
-        padding: 16
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        width: '80%',
+        paddingLeft: 20,
+        marginRight: 8
+    }
+
 });
